bab9f1013a318f9fc4abf0f7bf8484c442954c61
Valpaq <viktor.paramonov2000@gmail.com>
2023-01-10 17:38:37 +0300

    working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
+use num_bigint::ToBigInt;
 use sss::*;
-use num_bigint::{ ToBigInt};
 
 fn main() {
-    let secret = 100.to_bigint().unwrap();
-    let res = split(&secret, 10, 4);
+    let secret = 123435651345673413445647234u128.to_bigint().unwrap();
+    let res = split(&secret, 10, 3);
 
     for (index, key_part) in res.clone().into_iter().enumerate() {
-        println!("{}  {:x}", index + 1, key_part);
+        println!("{}  {:?}", index + 1, key_part);
     }
 
-    let res_secret = recover(vec![(0, res[0].clone()), (1, res[1].clone()), (2, res[2].clone()), (3, res[3].clone())]);
-    println!("{:?} ", res_secret);
+    let res_secret = recover(vec![(1, res[0].clone()), (2, res[1].clone()), (3, res[2].clone())]);
+    println!("{res_secret:?} ");
 }
--- src/recover.rs
+++ src/recover.rs
@@ -3,18 +3,8 @@ use num_bigint::{BigInt, ToBigInt};
 
 pub fn recover(points: Vec<(i128, BigInt)>) -> BigInt {
     let (points_x, _): (Vec<i128>, Vec<BigInt>) = points.iter().cloned().unzip();
-    let numerator = points.clone().iter().map(|(x_point, y_point)| {
-                        points_x
-                            .iter()
-                            .filter(|x| *x != x_point)
-                            .filter(|x| *x != &0)
-                            .fold(1, |res, a| res * a)
-                            .to_bigint()
-                            .unwrap()
-                }).collect::<Vec<BigInt>>();
-    println!("{:?} numerator", numerator);
-    println!("{:?} points_x", points_x);
-    let check: Vec<Fraction> = points
+
+    points
         .iter()
         .map(|(x_point, y_point)| {
             Fraction::new(
@@ -24,7 +14,7 @@ pub fn recover(points: Vec<(i128, BigInt)>) -> BigInt {
                             .iter()
                             .filter(|x| *x != x_point)
                             .filter(|x| *x != &0)
-                            .product::<i128>()
+                            .fold(1, |res, a| -res * a)
                             .to_bigint()
                             .unwrap(),
                     )
@@ -37,10 +27,8 @@ pub fn recover(points: Vec<(i128, BigInt)>) -> BigInt {
                     .to_bigint()
                     .unwrap(),
             )
-        }).collect();
-    println!("{:?} check", check);
-    
-    check.into_iter().reduce(|acc, e| acc.fraction_add(&e))
+        })
+        .reduce(|acc, e| acc.fraction_add(&e))
         .unwrap()
         .finalize()
 }
