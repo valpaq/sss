3f02abd59aaf0e1889867138f87949be90c2cd45
Valpaq <viktor.paramonov2000@gmail.com>
2023-01-20 02:46:58 +0300

    wip

---
+++ src/input_output.rs
@@ -0,0 +1,69 @@
+use itertools::free::join;
+use itertools::Itertools;
+use num_bigint::BigInt;
+use std::fmt::Error;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+pub fn split_write(out: &mut dyn Write, information: Vec<BigInt>) -> Result<(), Error> {
+    for (index, key_part) in information.into_iter().enumerate() {
+        out.write(format!("{}  {:x}", index + 1, key_part).as_bytes())
+            .expect("unable to write");
+    }
+    Ok(())
+}
+
+pub fn parse_split(file: PathBuf) -> (BigInt, u64, u64) {
+    let file_contents = fs::read_to_string(file).expect("Unable to read file");
+    let mut lines_iter = file_contents.lines();
+    let string_to_hide = BigInt::from_str(&join(
+        lines_iter
+            .next()
+            .unwrap()
+            .as_bytes()
+            .into_iter()
+            .map(|s| format!("{:03}", s)),
+        &"",
+    ))
+    .unwrap();
+    let (n_parts, k_parts): (u64, u64) = lines_iter
+        .next()
+        .unwrap()
+        .split(' ')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .map(|s| s.parse().unwrap())
+        .next_tuple()
+        .unwrap();
+
+    (string_to_hide, n_parts, k_parts)
+}
+
+// pub fn parse_recover() -> (usize, Vec<(i128, BigInt)>) {
+//     let mut t = String::new();
+//     io::stdin().read_line(&mut t).expect("Failed to read T");
+//     let t: usize = t.trim().parse().expect("Non number value T");
+
+//     let mut key_parts: Vec<BigInt> = Vec::new();
+//     let mut x: Vec<i128> = Vec::new();
+
+//     for _ in 0..t {
+//         let mut s = String::new();
+//         io::stdin().read_line(&mut s).expect("Failed to read key");
+
+//         let args: Vec<&str> = s.split_whitespace().into_iter().collect();
+
+//         x.push(args[0].parse().expect("Number expected"));
+
+//         let key_part = args[1].trim().trim_start_matches("0x");
+//         let key_part = BigInt::parse_bytes(key_part.as_bytes(), 16).expect("Failed to convert");
+
+//         key_parts.push(key_part);
+//     }
+
+//     let v: Vec<(i128, BigInt)> = x.into_iter().zip(key_parts.into_iter()).collect();
+
+//     (t, v)
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,54 @@
-use num_bigint::ToBigInt;
+mod input_output;
+use input_output::*;
 use sss::*;
+use std::fs::File;
+use std::io;
+use std::path::PathBuf;
+// use std::iter::Iterator;
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+struct Cli {
+    #[arg(short, long, value_name = "command")]
+    command: String,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    read_from: PathBuf,
+
+    /// Sets a custom config file
+    #[arg(short, long, value_name = "FILE")]
+    write_to: Option<PathBuf>,
+}
 
 fn main() {
-    let secret = 123435651345673413445647234u128.to_bigint().unwrap();
-    let res = split(&secret, 10, 3);
+    // let secret = 123435651345673413445647234u128.to_bigint().unwrap();
+    // let res = split(&secret, 10, 3);
 
-    for (index, key_part) in res.clone().into_iter().enumerate() {
-        println!("{}  {:?}", index + 1, key_part);
-    }
+    // for (index, key_part) in res.clone().into_iter().enumerate() {
+    //     println!("{}  {:x}", index + 1, key_part);
+    // }
 
-    let res_secret = recover(vec![(1, res[0].clone()), (2, res[1].clone()), (3, res[2].clone())]);
-    println!("{res_secret:?} ");
+    // let res_secret = recover(vec![(1, res[0].clone()), (2, res[1].clone()), (3, res[2].clone())]);
+    // println!("{res_secret:x} ");
+    let cli = Cli::parse();
+    match cli.command.as_str() {
+        "split" => {
+            let (key, n_parts, k_parts) = parse_split(cli.read_from);
+            let res = split(&key, n_parts, k_parts);
+            if let Some(write_to) = cli.write_to {
+                let mut file = File::options()
+                    .append(true)
+                    .open(write_to)
+                    .expect("no file");
+                split_write(&mut file, res).expect("cannot write to file");
+            } else {
+                split_write(&mut io::stdout(), res).expect("cannot write to stdout");
+            }
+        }
+        "recover" => {}
+        _ => {}
+    };
 }
